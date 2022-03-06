0ad6f2b5bfd30b84c76b61daf561b11fdba7a440
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2022-03-06 20:07:14 +0700

    sua doi lai

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 import BreadCrumb from "./components/breadcrumb/Breadcrumb";
+
 function App() {
-  return <BreadCrumb />;
+  const breadcrumbItems = [
+    {
+      label : "WAI-ARIA Authoring Practices 1.1",
+      href : "../../",
+    },
+    {
+      label : "Design Patterns",
+      href : "../../#aria_ex",
+    },
+    {
+      label : "Breadcrumb Pattern",
+      href : "../../#aria_ex/#breadcrumb",
+    },
+    {
+      label : "Breadcrumb Example",
+      href : "../../#aria_ex/#breadcrumb/index.html",
+    }
+  ] 
+ 
+  return <BreadCrumb breadcrumbItems={breadcrumbItems}/>;
 }
 
 export default App;
--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -2,30 +2,11 @@ import React from "react";
 import styles from "./Breadcrumb.module.css";
 import BreadcrumbItem from "./BreadcrumbItem";
 
-const breadcrumbItems = [
-  {
-    label : "WAI-ARIA Authoring Practices 1.1",
-    href : "../../",
-  },
-  {
-    label : "Design Patterns",
-    href : "../../#aria_ex",
-  },
-  {
-    label : "Breadcrumb Pattern",
-    href : "../../#aria_ex/#breadcrumb",
-  },
-  {
-    label : "Breadcrumb Example",
-    href : "../../#aria_ex/#breadcrumb/index.html",
-  }
-] 
-
-const Breadcrumb = () => {
+const Breadcrumb = (props) => {
   return (
     <nav aria-label="Breadcrumb" className={`${styles.breadcrumb}`}>
       <ol>
-        {breadcrumbItems.map(Element => <BreadcrumbItem href={Element.href} label={Element.label} key={Math.random()}/>)}
+        {props.breadcrumbItems.map(Element => <BreadcrumbItem href={Element.href} label={Element.label} key={Math.random()}/>)}
       </ol>
     </nav>
   );
