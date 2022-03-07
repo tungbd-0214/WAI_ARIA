503d5e5c4051b92dd60f588a80573871aff1d403
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2022-03-07 17:08:30 +0700

    Fix errors in Breadcrumb component

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -3,13 +3,14 @@ import styles from "./Breadcrumb.module.css";
 import BreadcrumbItem from "./BreadcrumbItem";
 
 const Breadcrumb = (props) => {
-  const lastIndex = props.items.length;
+  const lastIndex = props.items.length - 1;
   return (
     <nav aria-label="Breadcrumb" className={styles.breadcrumb}>
       <ol>
-        {props.items.map((item,index) => <BreadcrumbItem href={item.href} label={item.label} key={index} current={index+1 === lastIndex ? true : false  }/>)}
+        {props.items.map((item,index) => <BreadcrumbItem href={item.href} label={item.label} key={item.href} current={index === lastIndex}/>)}
       </ol>
     </nav>
   );
 };
+
 export default Breadcrumb;
